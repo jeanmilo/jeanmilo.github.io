7fb99e64b06d1a3b1f8a6d000ec169aa34568370
Claudia (Dia) <claudia.parlee@gmail.com>
2025-01-04 16:16:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// h1 typewriter effect
+function typeWriter(text) {
+    document.getElementById("header").innerText = "";
+    let index = 0;
+    let interval = setInterval(function () {
+        if (index < text.length) {
+            document.getElementById("header").innerHTML += text.charAt(index);
+            index++;
+        } else {
+            clearInterval(interval);
+        }
+    }, 60); // Interval of 100ms
+}
+
+const text = document.getElementById("header").innerText;
+typeWriter(text);
+
